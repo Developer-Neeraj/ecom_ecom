bdc71b30b74712c05b577d36d2d94d8f28a87813
Neeraj <59769298+Developer-Neeraj@users.noreply.github.com>
2024-12-30 22:33:52 +0530

    Update Collection.jsx

--- src/pages/Collection.jsx
+++ src/pages/Collection.jsx
@@ -118,7 +118,7 @@ const Collection = () => {
       {/* Right Side */}
       <div className='flex-1'>
         <div className='flex justify-between text-base sm:text-2xl mb-4'>
-          <Title text1={'All'} text2={'COLLECTIONS'} />
+          <Title text1={'All '} text2={'COLLECTIONS'} />
           {/* Product Sort */}
           <select onChange={(e) => setShortType(e.target.value)} className='border border-gray-300 text-sm px-2'>
             <option value='relavent'>Sort by: Relevant</option>
